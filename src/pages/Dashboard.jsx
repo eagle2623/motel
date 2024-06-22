e06d77e64c33bbc66299a88188e85474060ba3bb
Fardeen Khan <khan.fardeen.dhn@gmail.com>
2024-06-22 13:53:13 +0530

    connected supabase

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
-function Dashboard() {
+function Dashboard() {  
+
   return (
     <Row type="horizontal">
       <Heading as="h1">Dashboard</Heading>
